9df300797047529f8ffdc4fa3189df6be780daff
nakisa <nakisa.donnelly@gmail.com>
2021-03-08 17:55:32 -0800

    Session 6
    Simulated VR Hand animations.
    
    S.O.L.I.D Principles of scripting.
    
    Animation parameters (SetBool("ParameterName", value))
    
    Using the Unity manual for XR Input.
    
    Custom input axis in the Input Manager.
    
    Collecting information from our controllers.

---
+++ Assets/Scripts/SimHandAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimHandAnimator : MonoBehaviour
+{
+    private Animator simHandAnimator;
+    
+    void Start()
+    {
+        simHandAnimator = GetComponentInChildren<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            simHandAnimator.SetBool("IsClosing", true);
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            simHandAnimator.SetBool("IsClosing", false);
+        }
+    }
+}
---
+++ Assets/Scripts/VRInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRInput : MonoBehaviour
+{
+    public Hands hand = Hands.Left;
+    public float gripValue;
+
+
+    private string gripAxis;
+    
+
+
+    void Start()
+    {
+        gripAxis = $"{hand}Grip";
+    }
+
+    
+    void Update()
+    {
+        gripValue = Input.GetAxis(gripAxis);
+    }
+}
+
+[System.Serializable]
+public enum Hands
+{
+    Left,
+    Right
+}
